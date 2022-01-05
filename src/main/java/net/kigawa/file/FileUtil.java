c613786d2fda70b996316461202604a2705c0e0f
kigawa <kigawa.8390@gmail.com>
2022-01-06 08:37:11 +0900

    update: file

--- src/main/java/net/kigawa/file/FileUtil.java
+++ src/main/java/net/kigawa/file/FileUtil.java
@@ -4,6 +4,11 @@ import java.io.File;
 import java.nio.file.Paths;
 
 public class FileUtil {
+
+    public static File getRelativeFile(String... path) {
+        return getFile(Paths.get("").toAbsolutePath().toFile(), path);
+    }
+
     public static File getFile(File parent, String... path) {
         File file = parent;
         for (String name : path) {
@@ -11,6 +16,7 @@ public class FileUtil {
         }
         return file;
     }
+
     public static File getAbsolutFile() {
         return Paths.get("").toAbsolutePath().toFile();
     }
