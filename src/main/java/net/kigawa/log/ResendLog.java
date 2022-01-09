f61aa7bbfe539050ab2beeeca12ee63f195b2ac0
kigawa <kigawa.8390@gmail.com>
2022-01-10 00:21:22 +0900

    add: handler

---
+++ src/main/java/net/kigawa/log/ResendLog.java
@@ -0,0 +1,32 @@
+package net.kigawa.log;
+
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+public class ResendLog extends Handler {
+    private final Logger logger;
+    private final Level level;
+
+    public ResendLog(Logger logger, Level level) {
+        this.logger = logger;
+        this.level = level;
+        setFormatter(new EmptyFormatter());
+    }
+
+    @Override
+    public void publish(LogRecord record) {
+        logger.log(level, getFormatter().format(record));
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public void close() throws SecurityException {
+
+    }
+}
