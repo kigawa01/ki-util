d96ded9a0d0d440778e08e2410a516d0fabfd075
kigawa <kigawa.8390@gmail.com>
2023-10-16 15:47:51 +0900

    release: 3.0.0

--- buildSrc/src/main/kotlin/net.kigawa.kutil.kutil.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/net.kigawa.kutil.kutil.java-conventions.gradle.kts
@@ -30,52 +30,62 @@ group = ProjectConfig.GROUP
 kotlin {
   jvm {
   }
-}
 
+  val publicationsFromMainHost =
+    listOf(jvm()).map { it.name } + "kotlinMultiplatform"
 
-
-publishing {
-  publications {
-    create<MavenPublication>("mavenJava") {
-      from(findProject(":api")?.components?.get("kotlin"))
-      pom {
-        name.set("kutil")
-        description.set("utilities for java")
-        url.set("https://github.com/kigawa01/kutil-java/")
-        properties.set(
-          mapOf(
+  publishing {
+    publications {
+      matching { it.name in publicationsFromMainHost }.all {
+        val targetPublication = this@all
+        tasks.withType<AbstractPublishToMaven>()
+          .matching { it.publication == targetPublication }
+          .configureEach { onlyIf { findProperty("isMainHost") == "true" } }
+      }
+      create<MavenPublication>("mavenJava") {
+        from(findProject(":api")?.components?.get("kotlin"))
+        pom {
+          name.set("kutil")
+          description.set("utilities for java")
+          url.set("https://github.com/kigawa01/kutil-java/")
+          properties.set(
+            mapOf(
+            )
           )
-        )
-        licenses {
-          license {
-            name.set("MIT License")
-            url.set("http://www.opensource.org/licenses/mit-license.php")
+          licenses {
+            license {
+              name.set("MIT License")
+              url.set("http://www.opensource.org/licenses/mit-license.php")
+            }
+          }
+          developers {
+            developer {
+              id.set("net.kigawa")
+              name.set("kigawa")
+              email.set("contact@kigawa.net")
+            }
+          }
+          scm {
+            connection.set("scm:git:https://github.com/kigawa01/kutil-java.git")
+            developerConnection.set("scm:git:https://github.com/kigawa01/kutil-java.git")
+            url.set("https://github.com/kigawa01/kutil-java")
           }
         }
-        developers {
-          developer {
-            id.set("net.kigawa")
-            name.set("kigawa")
-            email.set("contact@kigawa.net")
-          }
-        }
-        scm {
-          connection.set("scm:git:https://github.com/kigawa01/kutil-java.git")
-          developerConnection.set("scm:git:https://github.com/kigawa01/kutil-java.git")
-          url.set("https://github.com/kigawa01/kutil-java")
+      }
+    }
+
+    repositories {
+      maven {
+        name = "OSSRH"
+        url = URI("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+        credentials {
+          username = System.getenv("MAVEN_USERNAME")
+          password = System.getenv("MAVEN_PASSWORD")
         }
       }
     }
   }
 
-  repositories {
-    maven {
-      name = "OSSRH"
-      url = URI("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-      credentials {
-        username = System.getenv("MAVEN_USERNAME")
-        password = System.getenv("MAVEN_PASSWORD")
-      }
-    }
-  }
 }
+
+
